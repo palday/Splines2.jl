37df7de68d75b10aee5d20a76e46c55d925a306c
Phillip Alday <me@phillipalday.com>
2026-03-12 19:34:36 -0500

    docstrings + cleanup

--- src/nspline.jl
+++ src/nspline.jl
@@ -46,30 +46,35 @@ function basis(ns::NSplineBasis{T}, x::T, ders::Int=0) where {T<:Real}
 end
 
 """
-    ns_(x :: Array{T,1}; <keyword arguments>) where T<:Real
+    NSplineBasis(x::AbstractVector{T};              
+                 boundary_knots::Union{Tuple{T,T},Nothing}=nothing,
+                 interior_knots::Union{AbstractVector{T},Nothing}=nothing,
+                 order::Int=4,
+                 intercept::Bool=false,
+                 df::Int=order - 3 + Int(intercept),
+                 knots::Union{AbstractVector{T},Nothing}=nothing) where {T<:Real}
 
-Calculate a basis for natural B-splines and return a function with signature
-`(x:: Array{T,1}; ders :: Int = 0)` for evaluation of `ders`
-derivative for the splines at `x`.
+Calculate a basis for natural B-splines and return a callable type
+for evaluating points in that basis.
 
-The keyword arguments include one of:
-1. `df`, possibly in combination with `intercept`
-2. `boundary_knots` and `interior_knots`
-3. `knots`
+# Keyword Arguments
+- `boundary_knots`: boundary knots
+- `interior_knots`: interior knots
+- `order`: order of the spline
+- `intercept`: bool for whether to include an intercept
+- `df`: degrees of freedom
+- `knots`: full set of knots (excluding repeats)
 
-# Arguments
-- `boundary_knots :: Union{Tuple{T,T},Nothing} = nothing`: boundary knots
-- `interior_knots :: Union{Array{T,1},Nothing} = nothing`: interior knots
-- `order :: Int = 4`: order of the spline
-- `intercept :: Bool = false`: bool for whether to include an intercept
-- `df :: Int = order - 1 + Int(intercept)`: degrees of freedom
-- `knots :: Union{Array{T,1}, Nothing} = nothing`: full set of knots (excluding repeats)
-- `center :: Union{T,Nothing} = nothing)`: value to center the splines
+# Keyword Arguments of Returned Callable
+- `center`: value to center the splines
+- `derivs`: derivatives of the splines
 
 # Examples
+
 ```jldoctest
-julia> Splines2.ns_(collect(0.0:0.2:1.0), df=3)(collect(0.0:0.2:1.0))
-6×3 Array{Float64,2}:
+julia> spline = NSplineBasis(x; df=3);
+julia> spline(0.0:0.2:1.0)
+6×3 Matrix{Float64}:
   0.0       0.0        0.0     
  -0.100444  0.409332  -0.272888
   0.102383  0.540852  -0.359235
@@ -78,41 +83,6 @@ julia> Splines2.ns_(collect(0.0:0.2:1.0), df=3)(collect(0.0:0.2:1.0))
  -0.142857  0.428571   0.714286
 ```
 """
-
-"""
-    ns(x :: Array{T,1}; <keyword arguments>) where T<:Real
-
-Calculate a basis for natural B-splines. 
-
-The keyword arguments include one of:
-1. `df`, possibly in combination with `intercept`
-2. `boundary_knots` and `interior_knots`
-3. `knots`
-
-# Arguments
-- `boundary_knots :: Union{Tuple{T,T},Nothing} = nothing`: boundary knots
-- `interior_knots :: Union{Array{T,1},Nothing} = nothing`: interior knots
-- `order :: Int = 4`: order of the spline
-- `intercept :: Bool = false`: bool for whether to include an intercept
-- `df :: Int = order - 1 + Int(intercept)`: degrees of freedom
-- `knots :: Union{Array{T,1}, Nothing} = nothing`: full set of knots (excluding repeats)
-- `center :: Union{T,Nothing} = nothing)`: value to center the splines
-- `ders :: Int = 0`: derivatives of the splines
-
-# Examples
-```jldoctest
-julia> Splines2.ns(collect(0.0:0.2:1.0), df=3)
-6×3 Array{Float64,2}:
-  0.0       0.0        0.0     
- -0.100444  0.409332  -0.272888
-  0.102383  0.540852  -0.359235
-  0.501759  0.386722  -0.172481
-  0.418872  0.327383   0.217745
- -0.142857  0.428571   0.714286
-```
-"""
-
-
 function NSplineBasis(x::AbstractVector{T};              
                       boundary_knots::Union{Tuple{T,T},Nothing}=nothing,
                       interior_knots::Union{AbstractVector{T},Nothing}=nothing,
@@ -131,16 +101,6 @@ function NSplineBasis(x::AbstractVector{T};
     return spline
 end
 
-function ns(x::AbstractVector{T};            
-            center::Union{T,Nothing}=nothing,
-            derivs::Int=0,
-            kwargs...) where {T<:Real}
-    
-     spline = NSplineBasis(x; kwargs...)
-
-    return spline(x; center, derivs)
-end
-
 function (spline::NSplineBasis{T})(x::AbstractVector; 
                                    derivs::Int=0, 
                                    center::Union{Number,Nothing}=nothing) where {T}
@@ -153,3 +113,49 @@ function (spline::NSplineBasis{T})(x::AbstractVector;
     end
     return b
 end
+
+"""
+   ns(x::AbstractVector{T};            
+            center::Union{T,Nothing}=nothing,
+            derivs::Int=0,
+            kwargs...) where {T <: Real}
+
+Calculate a basis for natural B-splines and return `x` expressed in that basis. 
+
+# Keyword arguments
+- `center`: value to center the splines
+- `derivs`: derivatives of the splines
+- Further keyword arguments are passed to `NSplineBasis`.
+
+# Arguments
+- `boundary_knots :: Union{Tuple{T,T},Nothing} = nothing`: boundary knots
+- `interior_knots :: Union{Array{T,1},Nothing} = nothing`: interior knots
+- `order :: Int = 4`: order of the spline
+- `intercept :: Bool = false`: bool for whether to include an intercept
+- `df :: Int = order - 1 + Int(intercept)`: degrees of freedom
+- `knots :: Union{Array{T,1}, Nothing} = nothing`: full set of knots (excluding repeats)
+
+# Examples
+```jldoctest
+julia> ns(0.0:0.2:1.0; df=3)
+
+6×3 Matrix{Float64}:
+  0.0       0.0        0.0     
+ -0.100444  0.409332  -0.272888
+  0.102383  0.540852  -0.359235
+  0.501759  0.386722  -0.172481
+  0.418872  0.327383   0.217745
+ -0.142857  0.428571   0.714286
+```
+"""
+function ns(x::AbstractVector{T};            
+            center::Union{T,Nothing}=nothing,
+            derivs::Int=0,
+            kwargs...) where {T<:Real}
+    
+     spline = NSplineBasis(x; kwargs...)
+
+    return spline(x; center, derivs)
+end
+
+# TODO add show methods
